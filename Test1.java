5ec70eaa7bbc06a179b101f5b021bfe625021ae0
qihua90 <qihua90@126.com>
2016-08-01 23:43:10 +0800

    修改Test1.java
    添加一个update()方法，返回一句话

--- Test1.java
+++ Test1.java
@@ -1,4 +1,10 @@
 Public class Test1{
         public Static void main(String[]args){
-        System.out.println("hello world");}
+        System.out.println("hello world");
+                System.out.println(update());
+        }
+        
+        public static String update(){
+                return "第一次使用添加修改的功能"；
+        }
 }
